2a525e61e973eaebdbbf1758d9757fdafa41ee76
sergey <sbarchshevskiy@gmail.com>
2021-06-03 15:39:29 -0400

    create artcile route/comp

---
+++ components/homeComponents/Articles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+export default function Articles() {
+
+  return(
+    <View>
+      <Text>articles</Text>
+    </View>
+  )
+}

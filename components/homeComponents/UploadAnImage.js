f2725d12d2323648651f35dd6cdb09c37323cb9d
sergey <sbarchshevskiy@gmail.com>
2021-06-08 12:46:38 -0400

    implement camera features

--- components/homeComponents/UploadAnImage.js
+++ components/homeComponents/UploadAnImage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 import { Camera } from 'expo-camera';
 
 export default function UploadAnImage() {
@@ -20,23 +20,38 @@ export default function UploadAnImage() {
     return <Text>No access to camera</Text>;
   }
   return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+    <View style={{ flex: 1}}>
+      <View style={styles.cameraContainer}>
+        <Camera
+          style={styles.fixedRatio}
+          type={type}
+          ratio={'1:1'}
+
+        />
+      </View>
+
+      <Button
+        title="Flip Image"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}>
+      </Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  cameraContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  fixedRatio: {
+    flex: 1,
+    aspectRatio: 1
+  }
+})
 
